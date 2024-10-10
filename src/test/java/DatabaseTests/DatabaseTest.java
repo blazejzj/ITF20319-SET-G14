f13e5be4da7acd03216169adc5e82363b211486a
blazejzj <opexrrator@gmail.com>
2024-10-10 09:10:32 +0200

    feat: Refactored test testing for real connection -> Red

--- src/test/java/DatabaseTests/DatabaseTest.java
+++ src/test/java/DatabaseTests/DatabaseTest.java
@@ -3,6 +3,10 @@ package DatabaseTests;
 import database.Database;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseTest {
@@ -13,7 +17,11 @@ public class DatabaseTest {
     @Test
     @DisplayName("Test if SQLite db file is created and connection made")
     public void testDatabaseConnection() {
-        assertTrue(database.connect(), "Connection method should return true");
+        try (Connection connection = database.connect()) {
+            assertNotNull(database.connect(), "Connection method should not be null");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
