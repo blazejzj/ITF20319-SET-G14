a5594f3fc5787d16b9a7b57baaca9cbe8e3422fc
blazejzj <opexrrator@gmail.com>
2024-10-10 09:35:30 +0200

    feat: testCreateTables test created currently -> Red

--- src/test/java/DatabaseTests/DatabaseTest.java
+++ src/test/java/DatabaseTests/DatabaseTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DatabaseTest {
@@ -22,6 +24,26 @@ public class DatabaseTest {
         } catch (SQLException e) {
             fail("Database connection failed!" + e.getMessage());
         }
+    }
+
+    @Test
+    @DisplayName("Project table is created if it does not exist")
+    public void testCreateTables() throws SQLException {
+
+        // mock the connection and statemnt objects from SQL
+        Connection mockConnection = mock(Connection.class);
+        Statement mockStatement = mock(Statement.class);
+
+        when(mockConnection.createStatement()).thenReturn(mockStatement); // mock the connnection
+
+        Database database1 = spy(new Database("test.db")); // spy on our db instance
+        doReturn(mockConnection).when(database1).connect();
+
+        database1.createTables(); // create necessary tables
+
+
+        verify(mockConnection).createStatement(); // make sure createStatement and Execute were called
+        verify(mockStatement, times(2)).execute(anyString());
     }
 
 }
