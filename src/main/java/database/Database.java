fc9a63175ddda42ca6d07f1aec08e4fa03cc0817
blazejzj <opexrrator@gmail.com>
2024-10-10 09:43:43 +0200

    feat: createTables method now creates real tables -> Green

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -10,6 +10,7 @@ public class Database {
     private final String dbName;
     private final String dbURL = "jdbc:sqlite:";
 
+
     public Database(String dbName) {
         this.dbName = dbName;
     }
@@ -21,9 +22,13 @@ public class Database {
     }
 
     public void createTables() throws SQLException {
+        // query for projectstable and taskstable -> create
+        String projectsTable = "CREATE TABLE IF NOT EXISTS Projects (id INTEGER PRIMARY KEY, title TEXT)";
+        String tasksTable = "CREATE TABLE IF NOT EXISTS Tasks (id INTEGER PRIMARY KEY, title TEXT)";
+
         try (Connection connec = connect(); Statement state = connec.createStatement()) {
-            state.execute("");
-            state.execute("");
+            state.execute(projectsTable);
+            state.execute(tasksTable);
         }
     }
 
