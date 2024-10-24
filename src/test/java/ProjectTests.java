f3201e1b1c4c251cb7918ab2fc24b148204aa162
debbieaan <111729438+debbieaan@users.noreply.github.com>
2024-10-24 10:34:42 +0200

    feat: added basic project setup + basic tests

--- src/test/java/ProjectTests.java
+++ src/test/java/ProjectTests.java
@@ -1,25 +1,104 @@
-import models.Project;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
 
 public class ProjectTests {
 
+    public Project mockProject = mock(Project.class);
+    Task mockTask = mock(Task.class);
+
     @Test
-    @DisplayName("Multipe created projects have their own unique ID")
-    public void controlUniqueID() {
+    @DisplayName("Add task to project")
+    public void testAddTask() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(mockTask);
+
         // Arrange
-        Project project = new Project("ProjectName", "TestingStuff hello");
-        Project project2 = new Project("ProjectName", "TestingStuff hello");
+        when(mockProject.getTasks()).thenReturn(tasks);
 
         // Act
-        int idProject1 = project.getId(); // supposed to be 0
-        int idProject2 = project2.getId(); // supposed to be 1
+        mockProject.addTask(mockTask);
 
-        System.out.println(idProject1);
-        System.out.println(idProject2);
-        // Assert
-        // If the two id's do not match -> Success
-        Assertions.assertNotEquals(idProject1, idProject2);
+        // Assertions
+        Assertions.assertTrue(mockProject.getTasks().contains(mockTask));
+
+    }
+
+    @Test
+    @DisplayName("Remove task from project")
+    public void testRemoveTask() {
+
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(mockTask);
+
+        // Arrange
+        when(mockProject.getTasks()).thenReturn(tasks);
+
+        // Act
+        mockProject.removeTask(mockTask);
+
+        // Assertions
+        Assertions.assertFalse(mockProject.getTasks().isEmpty());
+
+    }
+
+
+    @Test
+    @DisplayName("Find a task in project")
+    public void testFindTask() {
+
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(mockTask);
+
+        //Arrange
+        when(mockProject.getTasks()).thenReturn(tasks);
+
+        // Act
+        Task taskFound = mockProject.findTask(mockTask.getTitle());
+
+        // Assertions
+        Assertions.assertNotNull(taskFound);
+
+    }
+
+
+//    @Test
+//    @DisplayName("Check if task is done")
+//    public void testIfTaskIsDone() {
+//
+//        ArrayList<Task> tasks = new ArrayList<>();
+//        tasks.add(mockTask);
+//
+//        // Arrange
+//        when(mockProject.taskIsDone());
+//
+//        // Act
+//        Task taskDone = mockProject.taskIsDone();
+//
+//        // Assertions
+//        Assertions.assertTrue(mockProject.taskIsDone());
+//    }
+
+
+    @Test
+    @DisplayName("Get project summary")
+    public void testProjectSummary() {
+
+        // Arrange
+
+
+        // Act
+
+        // Assertions
     }
 }
