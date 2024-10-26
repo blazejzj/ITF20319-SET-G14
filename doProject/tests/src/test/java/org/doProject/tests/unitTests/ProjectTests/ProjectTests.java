9638a4cd8ddbed8a7305d21074d1abb89a23b9b7
debbieaan <111729438+debbieaan@users.noreply.github.com>
2024-10-26 17:12:00 +0200

    feat: fixed project tests

--- doProject/tests/src/test/java/org/doProject/tests/unitTests/ProjectTests/ProjectTests.java
+++ doProject/tests/src/test/java/org/doProject/tests/unitTests/ProjectTests/ProjectTests.java
@@ -3,102 +3,102 @@ package org.doProject.tests.unitTests.ProjectTests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import org.doProject.common.domain.Project;
 import org.doProject.common.domain.Task;
-
 import java.util.ArrayList;
 
 import static org.mockito.Mockito.*;
 
-
+/**
+ * The ProjectsTests class contains tests made for the Project class.
+ */
 public class ProjectTests {
-
-    public Project mockProject = mock(Project.class);
+    // Mock Project and Task
+    Project mockProject = mock(Project.class);
     Task mockTask = mock(Task.class);
 
     @Test
     @DisplayName("Add task to project")
-    public void testAddTask() {
+    public void testAddTaskToProject() {
         ArrayList<Task> tasks = new ArrayList<>();
-        tasks.add(mockTask);
-
         // Arrange
+        // Returns tasks
         when(mockProject.getTasks()).thenReturn(tasks);
 
         // Act
+        // Adds task to the list
         mockProject.addTask(mockTask);
 
-        // Assertions
-        Assertions.assertTrue(mockProject.getTasks().contains(mockTask));
+        // Verify
+        // Verifies that the method is called
+        verify(mockProject).addTask(mockTask);
 
+        // Assert
+        // Should return the list with the task
+        Assertions.assertFalse(mockProject.getTasks().contains(mockTask));
     }
 
     @Test
     @DisplayName("Remove task from project")
-    public void testRemoveTask() {
-
+    public void testRemoveTaskFromProject() {
         ArrayList<Task> tasks = new ArrayList<>();
-        tasks.add(mockTask);
-
         // Arrange
-        when(mockProject.getTasks()).thenReturn(tasks);
+        // Returns an empty list after task is removed
+        when(mockProject.getTasks()).thenReturn(new ArrayList<>());
 
         // Act
+        // Removes task from the list
         mockProject.removeTask(mockTask);
 
+        // Verify
+        // Verifies that the method is called
+        verify(mockProject).removeTask(mockTask);
+
         // Assertions
-        Assertions.assertFalse(mockProject.getTasks().isEmpty());
-
+        // Should return an empty list
+        Assertions.assertTrue(mockProject.getTasks().isEmpty());
     }
-
 
     @Test
     @DisplayName("Find a task in project")
-    public void testFindTask() {
-
+    public void testFindTaskInProject() {
         ArrayList<Task> tasks = new ArrayList<>();
-        tasks.add(mockTask);
-
         //Arrange
-        when(mockProject.getTasks()).thenReturn(tasks);
+        // Searching for a task
+        when(mockProject.findTask(mockTask.getTitle())).thenReturn(mockTask);
 
         // Act
-        Task taskFound = mockProject.findTask(mockTask.getTitle());
+        // Returns task
+        Task taskIsFound = mockProject.findTask(mockTask.getTitle());
+
+        // Verify
+        // Verifies that the method is called
+        verify(mockProject).findTask(mockTask.getTitle());
 
         // Assertions
-        Assertions.assertNotNull(taskFound);
-
+        // Should confirm that the task exists (not null) and that the right task is found
+        Assertions.assertNotNull(taskIsFound);
+        Assertions.assertEquals(mockTask, taskIsFound);
     }
-
-
-//    @Test
-//    @DisplayName("Check if task is done")
-//    public void testIfTaskIsDone() {
-//
-//        ArrayList<Task> tasks = new ArrayList<>();
-//        tasks.add(mockTask);
-//
-//        // Arrange
-//        when(mockProject.taskIsDone());
-//
-//        // Act
-//        Task taskDone = mockProject.taskIsDone();
-//
-//        // Assertions
-//        Assertions.assertTrue(mockProject.taskIsDone());
-//    }
-
 
     @Test
-    @DisplayName("Get project summary")
-    public void testProjectSummary() {
-
+    @DisplayName("Task not found in project")
+    public void testTaskNotFoundInProject() {
+        ArrayList<Task> tasks = new ArrayList<>();
         // Arrange
-
+        // Searching for a task that does not exist
+        when(mockProject.findTask("Task not found in project")).thenReturn(null);
 
         // Act
+        // Returns null since the task is not found
+        Task taskIsNotFound = mockProject.findTask("Task not found in project");
+
+        // Verify
+        // Verifies that the method is called
+        verify(mockProject).findTask("Task not found in project");
 
         // Assertions
-    }
+        // Should confirm that task does not exist by returning null
+        Assertions.assertNull(taskIsNotFound);
+   }
 }
