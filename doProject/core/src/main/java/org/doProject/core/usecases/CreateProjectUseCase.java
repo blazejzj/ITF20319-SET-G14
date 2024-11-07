3142ccb8318c5616e506bd563d0cc57666243e1f
blazejzj <opexrrator@gmail.com>
2024-11-07 14:10:27 +0100

    feat: implemented a new usecase for creating a project

---
+++ doProject/core/src/main/java/org/doProject/core/usecases/CreateProjectUseCase.java
@@ -0,0 +1,32 @@
+package org.doProject.core.usecases;
+
+import org.doProject.core.domain.Project;
+import org.doProject.core.dto.ProjectDTO;
+import org.doProject.core.port.ProjectRepository;
+
+public class CreateProjectUseCase {
+
+    private final ProjectRepository projectRepository;
+
+    public CreateProjectUseCase(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public ProjectDTO execute(ProjectDTO projectDTO, int userId) throws Exception{
+        if (projectDTO.getTitle() == null || projectDTO.getTitle().trim().isEmpty()) {
+            throw new IllegalArgumentException("Project name cant be empty");
+        }
+
+        Project project = new Project(
+                projectDTO.getTitle(),
+                projectDTO.getDescription(),
+                userId
+        );
+
+        // assign (genereate) id via database
+        int projectId = projectRepository.saveProject(project);
+        projectDTO.setId(projectId);
+        
+        return projectDTO;
+    }
+}
